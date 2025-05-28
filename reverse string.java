22c7b90dc8d35c63ba3d62e619d56b04bb4aff70
VARADARAJULU NAVYA <varadharajunavya@gmail.com>
2025-05-28 11:42:44 +0530

    Add files via upload

---
+++ reverse string.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String s="navya";
+        for(int i=s.length()-1;i>=0;i--){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
